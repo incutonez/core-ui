70999a52be29ef1f52b386c5979c4a4d94ed20da
Jef <incutonez@hotmail.com>
2024-09-23 16:08:17 -0400

    feat: Making a more proper release - Adding tailwind and theme to output - Making sure proper package.json properties are set for reading the package

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,3 @@
-import colors from "tailwindcss/colors";
+import tailwindConfig from "./src/tailwind.config";
 
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-	theme: {
-		fontFamily: {
-			sans: ["Open Sans"],
-		},
-		extend: {
-			// Additional font sizes without line heights
-			fontSize: {
-				1: "1rem",
-				1.25: "1.25rem",
-			},
-			colors: {
-				gray: {
-					b: colors.gray["400"],
-				},
-				select: {
-					200: colors.sky["200"],
-					600: colors.sky["600"],
-				},
-			},
-		},
-	},
-	plugins: [],
-};
-
+export default tailwindConfig;
