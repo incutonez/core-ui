0dd86604b5ffe18c2810122b78cc872a89ffa1e9
Jef <incutonez@hotmail.com>
2025-02-21 10:18:02 -0500

    feat: Updates and Additions (#17)
    - Adding FieldTextArea.vue
    - Migrating to Tailwind v4
    - Adding gh-pages build step, which includes the index.html... it was previously just using what was published, which stripped that out

--- src/components/TableColumn.vue
+++ src/components/TableColumn.vue
@@ -1,15 +1,14 @@
+<!--We can't create a custom wrapper column component yet.  Looks like it should be fixed in 3.48.0-->
+<!--Source: https://github.com/primefaces/primevue/issues/4940-->
+
 <template>
-	<TableColumn v-bind="getColumnProps(props)" />
+<!--	<TableColumn v-bind="getColumnProps(props)" />-->
 </template>
 
-<script setup lang="ts">
-/**
- * We can't create a custom wrapper column component yet.  Looks like it should be fixed in 3.48.0
- * Source: https://github.com/primefaces/primevue/issues/4940
- */
-import TableColumn from "primevue/column";
-import { ITableColumn } from "@/types/table";
-import { getColumnProps } from "@/utils/table";
+<!--<script setup lang="ts">-->
+<!--import TableColumn from "primevue/column";-->
+<!--import { ITableColumn } from "@/types/table";-->
+<!--import { getColumnProps } from "@/utils/table";-->
 
-const props = defineProps<ITableColumn>();
-</script>
+<!--const props = defineProps<ITableColumn>();-->
+<!--</script>-->
